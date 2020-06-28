9a46e8153e3504473ce85299e0d5ab7eb5f56b05
Abody <abody@malaysiakini.com>
2020-06-28 19:09:38 +0800

    .

---
+++ components/dashboard/Players.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = React.memo(() => {
+  return (
+    <div>
+      Dashboard Player
+    </div>
+  )
+})
+
+export default Dashboard
--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import globalContext from '../context'
+import DashboardPlayer from './Players'
+import DashboardOwner from './Owners'
 
 const Dashboard = React.memo(() => {
+  const { user } = useContext(globalContext)
+  if (!user) return null
   return (
     <div>
-      Dashboard
+      {
+        user.role === 'PLAYER' ? (
+          <DashboardPlayer />
+        ) : (
+          <DashboardOwner />
+        )
+      }
     </div>
   )
 })
