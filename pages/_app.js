e9c53274a0b2f9f09fe1c26716830dd03f6e71d4
Abody <abody@malaysiakini.com>
2020-06-28 17:34:37 +0800

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
-import React, { memo } from 'react'
+import React, { memo, useState, useEffect } from 'react'
+import GlobalContext from '../components/context'
 import Head from 'next/head'
+import axios from 'axios'
+import { useRouter } from 'next/router'
 
 const App = memo(({ Component, pageProps }) => {
+  const router = useRouter()
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      if (!user) {
+        // send axios request
+        if (router.route !== '/login' || router.router !== '/register' || router.router !== '/feature' || router.router !== '/') {
+          router.push('/')
+        }
+      } else {
+        if (router.route === '/login' || router.router === '/register') {
+          router.push('/dashboard')
+        }
+      }
+    })()
+  }, [user])
   return (
     <>
       <Head>
         <link rel='stylesheet' href='./style.css' />
       </Head>
-      <Component {...pageProps} />
+      <GlobalContext.Provider value={{ setUser, user }}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
     </>
   )
 })
---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from '../components/dashboard'
+
+const DashboardPage = () => {
+  return (
+    <Dashboard />
+  )
+}
+
+export default DashboardPage
