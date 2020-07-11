f077174b3f8ce42c36ca2b2004519db92a98f3fb
Abody <abody@malaysiakini.com>
2020-07-11 16:59:03 +0800

    WTF

---
+++ middleware/files.js
@@ -0,0 +1,22 @@
+const Multer = require('multer')
+const path = require('path')
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  fileFilter: (req, file, callback) => {
+    const mimetype = file.mimetype
+    
+    // const ext = path.extname(file.originalname);
+    // if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+    //     return callback(new Error('Only images are allowed'))
+    // }
+      if(mimetype !== 'image/jpeg' && mimetype !== 'image/jpeg' && mimetype !== 'image/jpeg' && mimetype !== 'image/jpeg') {
+        return callback(new Error('Only images are allowed'))
+    }
+    callback(null, true)
+},
+  // limits: { files: 1, fileSize:  2000000 }
+})
+
+module.exports = multer
+
