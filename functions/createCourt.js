d14e9f9118882d74b4507e9ee4101ef17a54b0bb
Eng_A <eng_alshugaaye@gmail.com>
2020-07-12 18:14:14 +0800

    court

--- functions/createCourt.js
+++ functions/createCourt.js
@@ -1,9 +1,13 @@
 // get court model 
+const  { v4: uuidv4 } = require('uuid')
 const Court = require('../model/court')
+
+
 
 // create function to store the court 
 const CreateCourt = async (courtobj)=>{
     try {
+        courtobj.id = uuidv4()
         const addCourt = Court(courtobj)
 
         await addCourt.save()
