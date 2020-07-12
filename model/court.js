20346c03de295cf3f9d42683d523d71ef0a52fa2
Eng_A <eng_alshugaaye@gmail.com>
2020-07-12 17:49:23 +0800

    Court

--- model/court.js
+++ model/court.js
@@ -2,6 +2,14 @@ const { Schema, model } = require('../config/mongo')
 
 
 const courtModel = new Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  id:{
+    type:String,
+    required:true
+  },
   centerId:{
     type:String,
     required:true
